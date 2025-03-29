059e8b45fa709c6376944952bc2ba1185cd4465c
Clifford Weinmann <clifford@weinmann.africa>
2025-03-29 21:25:41 +0400

    Improved arg handling

--- .github/actions/hello/entrypoint.sh
+++ .github/actions/hello/entrypoint.sh
@@ -1,12 +1,45 @@
-#!/bin/sh -l
+#!/usr/bin/bash -l
 
-echo "#args: $#" >&2
-while [ $# -gt 0 ]
+debug=0
+title=''
+
+while getopts 'w:t:d' opt
 do
-	echo "arg: [$1]" >&2
-	shift
+	case ${opt} in
+		w)
+			who="${OPTARG}"
+			;;
+		d)
+			debug=1
+			;;
+		t)
+			if [ ! -z "${OPTARG}" ]
+			then
+				title="${OPTARG} "
+			fi
+			;;
+		?)
+			echo "Invalid option: -${OPTARG}."
+			exit 1
+			;;
+	esac
 done
-echo "Hello $1"
-time=$(date)
+
+if [ -z "${who}" ]
+then
+	echo "Usage: hello -w <who> [-t <title>] [-d]"
+	exit 1
+fi
+
+if [ $debug ]
+then
+	echo "DEBUG: #args: $#" >&2
+	echo "DEBUG: who: [$who]" >&2
+	echo "DEBUG: title: [$title]" >&2
+fi
+
+echo "Hello ${title}${who}"
+
+time="$(date)"
 echo "time='${time}'" >> $GITHUB_OUTPUT
 echo "hostname='$(hostname)'" >> $GITHUB_OUTPUT
